8d16a2161d44a0ec72037b4b497d9bda8b07128f
peti <petikoch@gmail.com>
2016-11-18 10:43:38 +0100

    initial project structure

---
+++ src/main/java/challenge1/guava/ListenableFutureExample.java
@@ -0,0 +1,9 @@
+package challenge1.guava;
+
+public class ListenableFutureExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/main/scala/challenge1/akka_stream/AkkaStreamExample.scala
@@ -0,0 +1,9 @@
+package challenge1.akka_stream
+
+class AkkaStreamExample {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
