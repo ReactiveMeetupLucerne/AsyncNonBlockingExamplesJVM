efef69efa8d3811d8484a1372093cefd63c05ad2
Marcus Fihlon <marcus@fihlon.ch>
2016-11-29 20:13:42 +0100

    :construction: Added an example for challenge 1 using RxJava2

---
+++ src/main/java/challenge1/rxjava2/RxJava2Example.java
@@ -0,0 +1,63 @@
+package challenge1.rxjava2;
+
+import externalLegacyCodeNotUnderOurControl.PriceService;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static externalLegacyCodeNotUnderOurControl.PrintlnWithThreadname.println;
+import static java.lang.Thread.sleep;
+
+/**
+ * This example uses RxJava2.
+ *
+ * @author Andy Nyffenegger
+ * @author Marcus Fihlon, www.fihlon.ch
+ */
+public class RxJava2Example {
+
+    private static final int NUMBER_OF_SERVICE_CALLS = 10;
+
+    private final Set<PriceService> services;
+    private int price = 0;
+    private int count = 0;
+
+    public static void main(final String... args) throws InterruptedException {
+        new RxJava2Example().run();
+        sleep(10_000);
+    }
+
+    /**
+     * Create the price services.
+     */
+    private RxJava2Example() {
+        this.services = new HashSet<>();
+        for (int i = 0; i < NUMBER_OF_SERVICE_CALLS; i++) {
+            this.services.add(new PriceService());
+        }
+    }
+
+    /**
+     * Use RxJava2 to call the price services.
+     */
+    private void run() {
+        Flowable.fromIterable(services)
+                .flatMap((priceService -> Flowable.fromCallable(priceService::getPrice)
+                        .subscribeOn(Schedulers.io())))
+                .subscribe(this::collector);
+    }
+
+    /**
+     * Collect the answers and calculate the average price.
+     */
+    private synchronized void collector(final int price) {
+        this.price += price;
+        if (++this.count == NUMBER_OF_SERVICE_CALLS) {
+            println("The average price is: " + this.price / this.count);
+            System.exit(0);
+        }
+    }
+
+}
