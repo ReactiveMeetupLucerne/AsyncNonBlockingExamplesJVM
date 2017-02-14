a17cc4ef01d77f3f36293842c2510ac2c8d0b4a6
peti <petikoch@gmail.com>
2017-02-14 19:38:36 +0100

    :tada:  https://github.com/ReactiveMeetupLucerne/AsyncNonBlockingExamplesJVM/issues/6

--- src/main/java/challenge1/rxjava1/RxJavaObservableExample.java
+++ src/main/java/challenge1/rxjava1/RxJavaObservableExample.java
@@ -2,46 +2,25 @@ package challenge1.rxjava1;
 
 import externalLegacyCodeNotUnderOurControl.PriceService;
 import rx.Observable;
-import rx.observables.MathObservable;
 import rx.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
 
 import static externalLegacyCodeNotUnderOurControl.PrintlnWithThreadname.println;
 
 public class RxJavaObservableExample {
-    PriceService priceService;
 
     public static void main(String[] args) throws InterruptedException {
-        new RxJavaObservableExample().run();
-    }
+        Observable.just(
+                new PriceService(2),
+                new PriceService(3),
+                new PriceService(2)
+        )
+                .flatMap(priceService -> Observable.fromCallable(priceService::getPrice).subscribeOn(Schedulers.io()))
+                .reduce(0, (aPrice, anotherPrice) -> aPrice + anotherPrice)
+                .map(sumOfAllPrices -> (sumOfAllPrices / 3.0))
+                .subscribe(avg -> println("Average price " + avg));
 
-    public RxJavaObservableExample() {
-        priceService = new PriceService();
-    }
-
-    public void run() {
-        Observable<Integer> prices = Observable.range(1, 3)
-                .flatMap(id -> Observable.<Integer>create(s -> {
-                            s.onNext(priceService.getPrice());
-                            s.onCompleted();
-                        }).subscribeOn(Schedulers.computation())
-                );
-
-        MathObservable<Integer> m = MathObservable.from(prices);
-        m.averageInteger(o -> {
-            println("calculating with " + o);
-            return o;
-        })
-                .observeOn(Schedulers.io())
-                .subscribe(d -> println("AVERAGE is " + d));
-
-        sleep(20000);
-    }
-
-
-    private void sleep(int milis) {
-        try {
-            Thread.sleep(milis);
-        } catch (InterruptedException ex) {
-        }
+        TimeUnit.SECONDS.sleep(10);
     }
 }
