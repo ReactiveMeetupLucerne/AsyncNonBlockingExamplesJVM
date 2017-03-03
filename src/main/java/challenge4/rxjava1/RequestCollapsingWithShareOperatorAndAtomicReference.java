2fb334aa85678d652f6c0cd7ea8b2e20ba2bcf30
peti <petikoch@gmail.com>
2017-03-03 10:58:28 +0100

    Update RequestCollapsingWithShareOperatorAndAtomicReference: - run the CAS loop on the caller thread - call the price service with Schedulers.io() - parallelize again further processing of the shared price result

--- src/main/java/challenge4/rxjava1/RequestCollapsingWithShareOperatorAndAtomicReference.java
+++ src/main/java/challenge4/rxjava1/RequestCollapsingWithShareOperatorAndAtomicReference.java
@@ -12,84 +12,74 @@ import static externalLegacyCodeNotUnderOurControl.PrintlnWithThreadname.println
 
 public class RequestCollapsingWithShareOperatorAndAtomicReference {
 
-	public static void main(String[] args) throws InterruptedException {
-		PriceService originalPriceService = new PriceService(2);
-		CollapsingRequestPriceService collapsingRequestPriceService = new CollapsingRequestPriceService(originalPriceService);
+    public static void main(String[] args) throws InterruptedException {
+        PriceService originalPriceService = new PriceService(2);
+        CollapsingRequestPriceService collapsingRequestPriceService = new CollapsingRequestPriceService(originalPriceService);
 
-		collapsingRequestPriceService.getPrice()
-				.subscribeOn(Schedulers.io())
-				.subscribe(price -> println("1: " + price));
+        collapsingRequestPriceService.getPriceAsyncCollapsed()
+                .subscribe(price -> println("1: " + price));
 
-		TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(10);
 
-		collapsingRequestPriceService.getPrice()
-				.subscribeOn(Schedulers.io())
-				.subscribe(price -> println("2a: " + price));
-		collapsingRequestPriceService.getPrice()
-				.subscribeOn(Schedulers.io())
-				.subscribe(price -> println("2b: " + price));
+        collapsingRequestPriceService.getPriceAsyncCollapsed()
+                .subscribe(price -> println("2a: " + price));
+        collapsingRequestPriceService.getPriceAsyncCollapsed()
+                .subscribe(price -> println("2b: " + price));
 
-		TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(10);
 
-		collapsingRequestPriceService.getPrice()
-				.subscribeOn(Schedulers.io())
-				.subscribe(price -> println("3: " + price));
+        collapsingRequestPriceService.getPriceAsyncCollapsed()
+                .subscribe(price -> println("3: " + price));
 
-		TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(10);
 
-		collapsingRequestPriceService.getPrice()
-				.subscribeOn(Schedulers.io())
-				.subscribe(price -> println("4a: " + price));
-		collapsingRequestPriceService.getPrice()
-				.subscribeOn(Schedulers.io())
-				.subscribe(price -> println("4b: " + price));
-		collapsingRequestPriceService.getPrice()
-				.subscribeOn(Schedulers.io())
-				.subscribe(price -> println("4c: " + price));
-		collapsingRequestPriceService.getPrice()
-				.subscribeOn(Schedulers.io())
-				.subscribe(price -> println("4d: " + price));
-		collapsingRequestPriceService.getPrice()
-				.subscribeOn(Schedulers.io())
-				.subscribe(price -> println("4e: " + price));
+        collapsingRequestPriceService.getPriceAsyncCollapsed()
+                .subscribe(price -> println("4a: " + price));
+        collapsingRequestPriceService.getPriceAsyncCollapsed()
+                .subscribe(price -> println("4b: " + price));
+        collapsingRequestPriceService.getPriceAsyncCollapsed()
+                .subscribe(price -> println("4c: " + price));
+        collapsingRequestPriceService.getPriceAsyncCollapsed()
+                .subscribe(price -> println("4d: " + price));
+        collapsingRequestPriceService.getPriceAsyncCollapsed()
+                .subscribe(price -> println("4e: " + price));
 
-		TimeUnit.SECONDS.sleep(10);
-	}
+        TimeUnit.SECONDS.sleep(10);
+    }
 
-	static class CollapsingRequestPriceService {
+    static class CollapsingRequestPriceService {
 
-		private final PriceService delegate;
-		private final AtomicReference<Single<Integer>> ongoingDelegateRequestRef = new AtomicReference<>();
+        private final PriceService delegate;
+        private final AtomicReference<Single<Integer>> ongoingDelegateRequestRef = new AtomicReference<>();
 
-		CollapsingRequestPriceService(final PriceService delegate) {
-			this.delegate = delegate;
-		}
+        CollapsingRequestPriceService(final PriceService delegate) {
+            this.delegate = delegate;
+        }
 
-		public Single<Integer> getPrice() {
-			return Single.just(1)
-					.flatMap(tick -> {
-						println("Handling incoming request. Will collapse concurrent requests...");
+        Single<Integer> getPriceAsyncCollapsed() {
+            println("Handling incoming request. Will collapse concurrent requests...");
 
-						// compare and swap loop:
-						while (true) {
-							Single<Integer> ongoingDelegateRequest = ongoingDelegateRequestRef.get();
-							if (ongoingDelegateRequest != null) {
-								return ongoingDelegateRequest;
-							} else {
-								final Single<Integer> newOngoingRequestCandidate = Observable.fromCallable(() -> {
-									try {
-										return delegate.getPrice();
-									} finally {
-										ongoingDelegateRequestRef.set(null);
-									}
-								}).share() // here is the "trick"
-										.toSingle();
-								if (ongoingDelegateRequestRef.compareAndSet(null, newOngoingRequestCandidate)) {
-									return newOngoingRequestCandidate;
-								}
-							}
-						}
-					});
-		}
-	}
+            // compare and swap loop
+            while (true) {
+                Single<Integer> currentOngoingRequest = ongoingDelegateRequestRef.get();
+                if (currentOngoingRequest != null) {
+                    return currentOngoingRequest;
+                } else {
+                    final Single<Integer> newOngoingRequestCandidate = Observable.fromCallable(() -> {
+                        try {
+                            return delegate.getPrice();
+                        } finally {
+                            ongoingDelegateRequestRef.set(null);
+                        }
+                    }).subscribeOn(Schedulers.io())
+                            .share() // here is the "trick"
+                            .toSingle()
+                            .flatMap(price -> Single.just(price).subscribeOn(Schedulers.io())); // parallelize again further processing
+                    if (ongoingDelegateRequestRef.compareAndSet(null, newOngoingRequestCandidate)) {
+                        return newOngoingRequestCandidate;
+                    }
+                }
+            }
+        }
+    }
 }
