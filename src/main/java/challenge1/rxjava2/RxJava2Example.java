bd24b17b17add00f14b3d586927cb6020ab39751
peti <petikoch@gmail.com>
2017-03-03 10:38:24 +0100

    Updated RxJava2Example according comment from @TexasFred in https://github.com/ReactiveMeetupLucerne/AsyncNonBlockingExamplesJVM/issues/7
    Fixes #7

--- src/main/java/challenge1/rxjava2/RxJava2Example.java
+++ src/main/java/challenge1/rxjava2/RxJava2Example.java
@@ -4,65 +4,34 @@ import externalLegacyCodeNotUnderOurControl.PriceService;
 import io.reactivex.Flowable;
 import io.reactivex.schedulers.Schedulers;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.TimeUnit;
 
 import static externalLegacyCodeNotUnderOurControl.PrintlnWithThreadname.println;
 
 /**
- * This example uses RxJava2.
+ * This example uses RxJava2 {@link Flowable}s.
  *
  * @author Andy Nyffenegger
  * @author Marcus Fihlon, www.fihlon.ch
+ * @author Peti Koch
  */
 public class RxJava2Example {
 
-    private static final int NUMBER_OF_SERVICE_CALLS = 10;
-
-    private final ExecutorService executorService;
-    private final Set<PriceService> services;
-    private final AtomicInteger price;
-    private final AtomicInteger count;
-
-    /**
-     * Create the price services.
-     */
-    private RxJava2Example() {
-        this.executorService = Executors.newCachedThreadPool();
-        this.price = new AtomicInteger(0);
-        this.count = new AtomicInteger(0);
-        this.services = new HashSet<>();
-        for (int i = 0; i < NUMBER_OF_SERVICE_CALLS; i++) {
-            this.services.add(new PriceService(10 - i));
-        }
-    }
-
     public static void main(final String... args) throws InterruptedException {
-        new RxJava2Example().run();
+        int numberOfPriceServices = 3;
+
+        Flowable<PriceService> priceServices =
+                Flowable.range(1, numberOfPriceServices)
+                        .map(integer -> new PriceService());
+
+        priceServices
+                .flatMap(priceService -> Flowable.fromCallable(priceService::getPrice).subscribeOn(Schedulers.io()))
+                .reduce((number1, number2) -> number1 + number2)
+                .map(sum -> sum / (double) numberOfPriceServices)
+                .subscribe(avg -> println("The average price is: " + avg));
+
+        TimeUnit.SECONDS.sleep(10);
     }
 
-    /**
-     * Use RxJava2 to call the price services.
-     */
-    private void run() {
-        Flowable.fromIterable(services)
-                .flatMap(priceService -> Flowable.fromCallable(priceService::getPrice)
-                        .subscribeOn(Schedulers.from(this.executorService)))
-                .subscribe(this::collector);
-        this.executorService.shutdown();
-    }
-
-    /**
-     * Collect the answers and calculate the average price.
-     */
-    private void collector(final int price) {
-        this.price.addAndGet(price);
-        if (this.count.incrementAndGet() == NUMBER_OF_SERVICE_CALLS) {
-            println("The average price is: " + this.price.get() / this.count.get());
-        }
-    }
 
 }
